0aefe4b8923a6da49e7dd87ef27b838e14dfc1cd
rei.yasuno <matsuyama.u.ry@gmail.com>
2018-07-12 15:17:48 +0900

    Add CreateServlet.java

--- src/contorollers/CreateServlet.java
+++ src/contorollers/CreateServlet.java
@@ -1,11 +1,17 @@
 package contorollers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class CreateServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/create")
 public class CreateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,6 +33,31 @@ public class CreateServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	    String _token = (String)request.getParameter("_token");
+	    if(_token != null && _token.equals(request.getSession().getId())) {
+	        EntityManager em = DBUtil.createEntityManager();
+
+	        Task t = new Task();
+
+	        String title = request.getParameter("title");
+	        t.setTitle(title);
+
+	        String content = request.getParameter("content");
+
+	        String other = request.getParameter("other");
+
+	        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+	        t.setCreated_at(currentTime);
+	        t.setUpdated_at(currentTime);
+
+	        em.getTransaction().begin();
+	        em.persist(t);
+	        em.getTransaction().commit();
+	        em.close();
+
+	        response.sendRedirect(request.getContextPath() + "/index");
+	    }
+
 	}
 
 }
