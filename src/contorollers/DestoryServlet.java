6cbcbc7593dc10583daa7241a792cf7726e7188a
rei.yasuno <matsuyama.u.ry@gmail.com>
2018-07-15 22:19:47 +0900

    Add DestroyServlet.java

--- src/contorollers/DestoryServlet.java
+++ src/contorollers/DestoryServlet.java
@@ -1,11 +1,16 @@
 package contorollers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class DestoryServlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/destory")
 public class DestoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,6 +32,22 @@ public class DestoryServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	    String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            em.getTransaction().begin();
+            em.remove(t);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("task_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
 	}
 
 }
